7f519000423610cd8c4ceca2687d1a4d18b5234f
Pouya Rad <pouyarad15@gmail.com>
2025-12-10 21:10:12 -0500

    added simplyPIController

--- drive.py
+++ drive.py
@@ -15,3 +15,29 @@ import eventlet.wsgi
 from tensorflow.keras.models import load_model
 from data_preprocess import preprocess_image
 
+# Socket.IO server + Flask
+sio = socketio.Server(logger=False, async_mode="eventlet")
+flask_app = Flask(__name__)
+app = socketio.Middleware(sio, flask_app)
+
+model = None
+args = None
+
+
+class SimplePIController:
+    def __init__(self, Kp, Ki):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.set_point = 0.0
+        self.error = 0.0
+        self.integral = 0.0
+
+    def set_desired(self, desired):
+        self.set_point = desired
+
+    def update(self, measurement):
+        self.error = self.set_point - measurement
+        self.integral += self.error
+        return self.Kp * self.error + self.Ki * self.integral
+
+
